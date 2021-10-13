9c6be56409432ef119e2a8240516c3aaa4958b98
Artur Szerejko <szerejko.artur@gmail.com>
2021-10-13 16:16:48 -0400

    functional C4 button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import * as Tone from 'tone';
+import { PitchClass } from './utils/types';
 
 const App = () => {
+	const synth = new Tone.Synth().toDestination();
+
+	const playNote = (note: PitchClass): void => {
+		synth.triggerAttackRelease(`${note}4`, '8n');
+	};
+
 	return (
 		<div>
 			<header></header>
 			<div>
-				<button>play me</button>
+				<button className="note" onClick={() => playNote(PitchClass.C)}>
+					C
+				</button>
 			</div>
 		</div>
 	);
