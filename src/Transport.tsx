624c1c72970935a29ef122ca67425962d39fc3c3
Artur Szerejko <szerejko.artur@gmail.com>
2021-10-21 16:22:21 -0400

    refactored to toggle, tone's audio context initialized on play, refactored multiple voices but no polyphony

--- src/Transport.tsx
+++ src/Transport.tsx
@@ -2,35 +2,38 @@ import * as Tone from 'tone';
 import { useState, useEffect } from 'react';
 
 import Voice from './Voice';
+import { PitchClass } from './utils/types';
 
 const Transport = () => {
 	const [bpm, setBpm] = useState(120);
-	const [period, setPeriod] = useState(Tone.Time('2m').toSeconds());
+	const [period, setPeriod] = useState(Tone.Time('1m').toSeconds());
+
+	const [triggerText, setTriggerText] = useState('play');
 
 	useEffect(() => {
 		Tone.Transport.cancel();
 		Tone.Transport.bpm.value = bpm;
-		setPeriod(Tone.Time('2m').toSeconds());
+		setPeriod(Tone.Time('1m').toSeconds());
 	}, [bpm]);
 
 	useEffect(() => {
 		Tone.Transport.cancel();
-
 		Tone.Transport.loopStart = 0;
 		Tone.Transport.loopEnd = period;
 		Tone.Transport.loop = true;
 	}, [bpm, period]);
 
-	const initialize = () => {
-		Tone.start();
-	};
+	const triggerLoop = async () => {
+		if (Tone.context.state === 'suspended') {
+			await Tone.start();
+			console.log(`context resumed`);
+		}
 
-	const triggerLoop = () => {
-		Tone.Transport.start();
-	};
-
-	const stopLoop = () => {
-		Tone.Transport.stop();
+		Tone.Transport.toggle();
+		triggerText === 'play'
+			? setTriggerText('stop')
+			: setTriggerText('play');
+		console.log(Tone.Transport.state);
 	};
 
 	const scheduleEvents = (
@@ -49,9 +52,7 @@ const Transport = () => {
 
 	return (
 		<div>
-			<button onClick={() => initialize()}>initialize</button>
-			<button onClick={() => triggerLoop()}>play</button>
-			<button onClick={() => stopLoop()}>stop</button>
+			<button onClick={() => triggerLoop()}>{triggerText}</button>
 			<div>
 				{'bpm:'}
 				<input
@@ -59,7 +60,18 @@ const Transport = () => {
 					onChange={(event) => setBpm(parseInt(event.target.value))}
 				/>
 			</div>
-			<Voice period={period} scheduleEvents={scheduleEvents} />
+			<Voice
+				period={period}
+				scheduleEvents={scheduleEvents}
+				voice={1}
+				pitch={PitchClass.C}
+			/>
+			<Voice
+				period={period}
+				scheduleEvents={scheduleEvents}
+				voice={2}
+				pitch={PitchClass.G}
+			/>
 			<div>{`length of period in seconds: ${period}`}</div>
 		</div>
 	);
--- src/Voice.tsx
+++ src/Voice.tsx
@@ -1,12 +1,15 @@
 import * as Tone from 'tone';
 import { useState, useEffect } from 'react';
+import { PitchClass } from './utils/types';
 
 type VoiceProps = {
 	period: number;
 	scheduleEvents: (interval: number, callback: (time: any) => void) => void;
+	voice: number;
+	pitch: PitchClass;
 };
 
-const Voice = ({ period, scheduleEvents }: VoiceProps) => {
+const Voice = ({ period, scheduleEvents, voice, pitch }: VoiceProps) => {
 	const [numOfSteps, setNumOfSteps] = useState(4);
 	const [interval, setInterval] = useState(1);
 
@@ -21,14 +24,14 @@ const Voice = ({ period, scheduleEvents }: VoiceProps) => {
 		Tone.Transport.cancel();
 		setInterval(period / numOfSteps);
 		scheduleEvents(interval, (time) =>
-			synth.triggerAttackRelease('C4', interval * 0.75, time)
+			synth.triggerAttackRelease(`${pitch}4`, interval * 0.75, time)
 		);
 	}, [period, interval]);
 
 	return (
 		<div>
 			<div>
-				{'num of steps:'}
+				{`Voice ${voice}: num of steps-`}
 				<input
 					value={numOfSteps}
 					onChange={(event) =>
