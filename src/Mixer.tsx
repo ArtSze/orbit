1f735b82a1aa9cc6c496872e2bc184125e4c5bc8
Artur Szerejko <szerejko.artur@gmail.com>
2021-12-21 14:40:41 -0500

    mixer components memoized, no more stuttering when switching between tonal and non-tonal

--- src/Mixer.tsx
+++ src/Mixer.tsx
@@ -1,42 +1,69 @@
 import * as Tone from 'tone';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 import Transport from './Transport';
-import Container from '@mui/material/Container';
 
 const Mixer = () => {
 	const [tonal, setTonal] = useState(true);
 
-	const limiter = new Tone.Limiter(-5).toDestination();
+	const limiter = useMemo(() => {
+		return new Tone.Limiter(-5).toDestination();
+	}, []);
 
-	const channel1 = new Tone.Channel().connect(limiter);
-	const channel2 = new Tone.Channel().connect(limiter);
-	const channel3 = new Tone.Channel().connect(limiter);
+	const channel1 = useMemo(() => {
+		return new Tone.Channel().connect(limiter);
+	}, []);
+	const channel2 = useMemo(() => {
+		return new Tone.Channel().connect(limiter);
+	}, []);
+	const channel3 = useMemo(() => {
+		return new Tone.Channel().connect(limiter);
+	}, []);
 
-	const synth1 = new Tone.Synth().connect(channel1);
-	const synth2 = new Tone.Synth().connect(channel2);
-	const synth3 = new Tone.Synth().connect(channel3);
+	const synth1 = useMemo(() => {
+		return new Tone.Synth().connect(channel1);
+	}, []);
+	const synth2 = useMemo(() => {
+		return new Tone.Synth().connect(channel2);
+	}, []);
+	const synth3 = useMemo(() => {
+		return new Tone.Synth().connect(channel3);
+	}, []);
 
-	const player1 = new Tone.Player(
-		'https://archive.org/download/JeanLucCohen/JeanLucCohen22.zip/JeanLucCohen22%2FKick_15.wav'
-	).connect(channel1);
-	const player2 = new Tone.Player(
-		'https://archive.org/download/JeanLucCohen/JeanLucCohen22.zip/JeanLucCohen22%2FSnare_11.wav'
-	).connect(channel2);
-	const player3 = new Tone.Player(
-		'https://archive.org/download/JeanLucCohen/JeanLucCohen22.zip/JeanLucCohen22%2FHat_01.wav'
-	).connect(channel3);
+	const player1 = useMemo(() => {
+		return new Tone.Player(
+			'https://archive.org/download/JeanLucCohen/JeanLucCohen22.zip/JeanLucCohen22%2FKick_15.wav'
+		).connect(channel1);
+	}, []);
+	const player2 = useMemo(() => {
+		return new Tone.Player(
+			'https://archive.org/download/JeanLucCohen/JeanLucCohen22.zip/JeanLucCohen22%2FSnare_11.wav'
+		).connect(channel2);
+	}, []);
+	const player3 = useMemo(() => {
+		return new Tone.Player(
+			'https://archive.org/download/JeanLucCohen/JeanLucCohen22.zip/JeanLucCohen22%2FHat_01.wav'
+		).connect(channel3);
+	}, []);
 
 	player1.volume.value = 4;
 	player2.volume.value = 0;
 	player3.volume.value = -7;
 
-	const chorus = new Tone.Chorus(4, 2.5, 0.5).start().connect(limiter);
-	const chorusChannel = new Tone.Channel({ volume: -60 }).connect(chorus);
+	const chorus = useMemo(() => {
+		return new Tone.Chorus(4, 2.5, 0.5).start().connect(limiter);
+	}, []);
+	const chorusChannel = useMemo(() => {
+		return new Tone.Channel({ volume: -60 }).connect(chorus);
+	}, []);
 	chorusChannel.receive('chorus');
 
-	const crusher = new Tone.BitCrusher(6).connect(limiter);
-	const crusherChannel = new Tone.Channel({ volume: -60 }).connect(crusher);
+	const crusher = useMemo(() => {
+		return new Tone.BitCrusher(6).connect(limiter);
+	}, []);
+	const crusherChannel = useMemo(() => {
+		return new Tone.Channel({ volume: -60 }).connect(crusher);
+	}, []);
 	crusherChannel.receive('crusher');
 
 	channel1.send('chorus');
